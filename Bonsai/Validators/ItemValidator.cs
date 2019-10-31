034a9eda9e444153e998f9e86888091a0ff0eeb0
anaspoiala <ana.spoiala25@gmail.com>
2019-10-31 15:27:57 +0200

    Added new validator classes. Renamed Validator to TextValidator.

---
+++ Bonsai/Validators/ItemValidator.cs
@@ -0,0 +1,34 @@
+﻿using Bonsai.Domain;
+
+namespace Bonsai.Validators
+{
+    public static class ItemValidator
+    {
+
+        public static bool NameIsEmpty(Item item)
+        {
+            return string.IsNullOrWhiteSpace(item.Name);
+        }
+
+        public static bool QuantityIsEmpty(Item item)
+        {
+            return item.Quantity == null;
+        }
+
+        public static bool BuyDateIsEmpty(Item item)
+        {
+            return item.BuyDate == null; 
+        }
+
+        public static bool ExpirationDateIsEmpty(Item item)
+        {
+            return item.ExpirationDate == null;
+        }
+
+        public static bool ItemIsExpired(Item item)
+        {
+            return item.ExpirationDate.Value.CompareTo(System.DateTime.UtcNow) >= 0;
+        }
+
+    }
+}
---
+++ Bonsai/Validators/TextValidator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Bonsai.Validators
+{
+    public static class TextValidator
+    {
+        private static readonly string EMAIL_PATTERN = @"(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])";
+        private static readonly string SYMBOL_PATTERN = @".*[!\(\)\-\?.,\[\]_'""`~#$%^&*+=]+.*";
+
+        public static bool IsNullEmptyOrWhiteSpace(string word)
+        {
+            return string.IsNullOrWhiteSpace(word);
+        }
+
+        public static bool ContainsLetters(string word)
+        {
+            return word.Any(c => char.IsLetter(c));
+        }
+
+        public static bool ContainsLowerLetters(string word)
+        {
+            return word.Any(c => char.IsLower(c));
+        }
+
+        public static bool ContainsUpperLetters(string word)
+        {
+            return word.Any(c => char.IsUpper(c));
+        }
+
+        public static bool ContainsNumbers(string word)
+        {
+            return word.Any(c => char.IsNumber(c));
+        }
+
+        public static bool ContainsSymbols(string word)
+        {
+            //return word.Any(c => char.IsSymbol(c));
+            return Regex.IsMatch(word, SYMBOL_PATTERN);
+        }
+
+        public static bool FirstCharacterIsSymbol(string word)
+        {
+            //return word.Any(c => char.IsSymbol(c));
+            return Regex.IsMatch(word.Substring(0, 1), SYMBOL_PATTERN);
+        }
+
+        public static bool ContainsBetweenXAndYCharacters(string word, int min, int max)
+        {
+            return (word.Length >= min && word.Length <= max);
+        }
+
+        public static bool HasEmailFormat(string word)
+        {
+            return Regex.IsMatch(word, EMAIL_PATTERN);
+        }
+        
+
+    }
+}
